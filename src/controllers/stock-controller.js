fb465621e56ab3f4ff54c20c53389f8e6f990904
Adriano Berto <adriano.berto@outlook.com>
2021-09-16 01:29:53 -0300

    inclusão de exception no retono da busca de stocks

--- src/controllers/stock-controller.js
+++ src/controllers/stock-controller.js
@@ -8,6 +8,7 @@ exports.listStocks = async (req, res) => {
         const data = await repository.listStocks();
         return res.status(200).send(data);
     } catch (e) {
+        throw e;
         console.log(e);
         return res.status(500).send({ message: 'Falha ao carregar as menções!', error: e });
     }
